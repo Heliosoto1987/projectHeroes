464ab4baf0830f5b4f24452bd27590085dab46d0
Helio <heliosoto17@gmail.com>
2021-06-22 00:45:08 +0200

    add build docs

---
+++ docs/precache-manifest.d62fb3edf07215b603566e154accf20c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f586dd7c956d48845732cf005e61f7d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17bc669a1209192fa5f3",
+    "url": "/static/js/2.b546d312.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b546d312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3744d4567ac46a44500",
+    "url": "/static/js/main.b56882c4.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  }
+]);
